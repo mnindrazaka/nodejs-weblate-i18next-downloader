61d506402dd01a41c0ec567a9d98a309b52d380f
M. Nindra Zaka <mnindrazaka@gmail.com>
2023-07-07 16:49:09 +0700

    refactor: add weblate url

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const AdmZip = require("adm-zip");
 const fs = require("fs-extra");
 const path = require("path");
 
+const WEBLATE_URL = "http://localhost";
 const PROJECT_NAME = "karirlab-frontend";
 const EXCLUDED_NAMESPACE = "glossary";
 const LOCALES_PATH = path.join(__dirname, "public", "locales");
 const WEBLATE_EXTRACT_PATH = path.join(LOCALES_PATH, PROJECT_NAME);
-const DOWNLOAD_URL = `http://localhost/download/${PROJECT_NAME}`;
+const DOWNLOAD_URL = `${WEBLATE_URL}/download/${PROJECT_NAME}`;
 
 async function downloadAndExtractZip(url, outputPath) {
   try {
