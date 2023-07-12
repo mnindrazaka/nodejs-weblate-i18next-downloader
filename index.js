64bc64d3c37a76bd6675f714775a30a7c9b896d8
M. Nindra Zaka <mnindrazaka@gmail.com>
2023-07-12 14:32:35 +0700

    feat: update script using api

--- index.js
+++ index.js
@@ -1,75 +1,71 @@
 const axios = require("axios").default;
-const AdmZip = require("adm-zip");
 const fs = require("fs-extra");
 const path = require("path");
+require("dotenv").config();
 
-const WEBLATE_URL = "http://localhost";
-const PROJECT_NAME = "karirlab-frontend";
-const EXCLUDED_NAMESPACE = "glossary";
-const LOCALES_PATH = path.join(__dirname, "public", "locales");
-const WEBLATE_EXTRACT_PATH = path.join(LOCALES_PATH, PROJECT_NAME);
-const DOWNLOAD_URL = `${WEBLATE_URL}/download/${PROJECT_NAME}`;
+const apiClient = axios.create({
+  baseURL: process.env.API_URL,
+  headers: { Authorization: `Token ${process.env.API_KEY}` },
+});
 
-async function downloadAndExtractZip(url, outputPath) {
-  try {
-    const response = await axios.get(url, { responseType: "arraybuffer" });
-    const zip = new AdmZip(response.data);
-    zip.extractAllTo(outputPath, true);
-  } catch (err) {
-    throw err;
-  }
+const PROJECT_NAME = process.env.PROJECT_NAME;
+
+function getLanguages() {
+  return apiClient
+    .get(`/projects/${PROJECT_NAME}/languages/?format=json`)
+    .then((response) => response.data);
 }
 
-function getNameSpaces() {
-  return fs
-    .readdirSync(WEBLATE_EXTRACT_PATH)
-    .filter((namespace) => namespace !== EXCLUDED_NAMESPACE);
-}
-
-function getLanguages(namespaces) {
-  const languagesMap = {};
-  namespaces.forEach((namespace) => {
-    const namespaceLanguages = fs.readdirSync(
-      path.join(WEBLATE_EXTRACT_PATH, namespace, namespace)
+function getComponents() {
+  return apiClient
+    .get(`/projects/${PROJECT_NAME}/components/?format=json`)
+    .then((response) =>
+      response.data.results.filter((component) => component.slug !== "glossary")
     );
-    namespaceLanguages.forEach((languageKey) => {
-      const language = languageKey.split(".")[0];
-      languagesMap[language] = true;
-    });
-  });
-
-  const languages = Object.keys(languagesMap);
-  return languages;
 }
 
-function restructureFile(languages, namespaces) {
-  languages.forEach((language) => {
-    if (!fs.existsSync(path.join(LOCALES_PATH, language))) {
-      fs.mkdirSync(path.join(LOCALES_PATH, language));
-    }
-
-    namespaces.forEach((namespace) => {
-      const data = fs.readFileSync(
+function downloadTranslationFile({ componentSlug, languageCode }) {
+  apiClient
+    .get(
+      `/translations/${PROJECT_NAME}/${componentSlug}/${languageCode}/file/`,
+      { responseType: "stream" }
+    )
+    .then((response) => {
+      const writer = fs.createWriteStream(
         path.join(
-          WEBLATE_EXTRACT_PATH,
-          namespace,
-          namespace,
-          language + ".json"
+          __dirname,
+          "public",
+          "locales",
+          languageCode,
+          `${componentSlug}.json`
         )
       );
-      fs.writeFileSync(
-        path.join(LOCALES_PATH, language, namespace + ".json"),
-        data
-      );
+      response.data.pipe(writer);
     });
-  });
 }
 
-function main() {
-  downloadAndExtractZip(DOWNLOAD_URL, LOCALES_PATH);
-  const namespaces = getNameSpaces();
-  const languages = getLanguages(namespaces);
-  restructureFile(languages, namespaces);
+async function main() {
+  const LOCALES_PATH = path.join(__dirname, "public", "locales");
+  fs.emptyDirSync(LOCALES_PATH);
+
+  const languages = await getLanguages();
+  const components = await getComponents();
+
+  languages.forEach((language) => {
+    fs.mkdirSync(path.join(LOCALES_PATH, language.code), {
+      recursive: true,
+    });
+
+    components.forEach((component) => {
+      console.log(
+        `[translation] Downloading ${language.code}/${component.slug}.json`
+      );
+      downloadTranslationFile({
+        componentSlug: component.slug,
+        languageCode: language.code,
+      });
+    });
+  });
 }
 
 main();
